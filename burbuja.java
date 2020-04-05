a7cedac5bb0e0cbd2aea8708a739a1ec0ff6bddb
edgar-adrian-reyes-miranda <58642547+edgar-adrian-reyes-miranda@users.noreply.github.com>
2020-04-05 05:36:26 -0500

    Add files via upload

---
+++ burbuja.java
@@ -0,0 +1,42 @@
+package ordenamiento;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class burbuja {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+     
+        System.out.print("\n Ingrese Numero de Datos a Ingresar : ");
+        int tam = Integer.parseInt(in.readLine());
+       
+        int arr[] = new int[tam];
+        System.out.println();
+    
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            j += 1;
+            System.out.print("Elemento " + j + " : ");
+            arr[i] = Integer.parseInt(in.readLine());
+        }
+        burbuja(arr);
+    } @Override
+    public String toString() {
+        return "burbuja{" + '}';
+    } static void burbuja(int arreglo[]) {
+        for (int i = 0; i < arreglo.length - 1; i++) {
+            for (int j = 0; j < arreglo.length - 1; j++) {
+                if (arreglo[j] < arreglo[j + 1]) {
+                    int tmp = arreglo[j + 1];
+                    arreglo[j + 1] = arreglo[j];
+                    arreglo[j] = tmp;
+                }
+            }
+        }
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.print(arreglo[i] + "\n");
+        }
+    }
+}
